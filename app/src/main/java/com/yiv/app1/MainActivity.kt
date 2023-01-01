a482b9472ab3ad5ef17798abd6d2ffe5ad78f4d1
YuRa <ra2net@ya.ru>
2023-01-01 15:01:08 +0300

    git commit

--- app/src/main/java/com/yiv/app1/MainActivity.kt
+++ app/src/main/java/com/yiv/app1/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.yiv.app1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.yiv.app1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+
+            val emailText = binding.editTextTextEmailAddress.text.toString()
+            val passText = binding.editTextTextPassword.text.toString()
+
+            if (emailText == "ra@admin.ru" && passText == "123"){
+                showToast("Хорошо")
+            }
+            else {
+                showToast("Плохо")
+            }
+
+        }
+
     }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+
 }
